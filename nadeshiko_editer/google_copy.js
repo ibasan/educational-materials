d427ce5b6919ca7f9b878401dce9d4527d585a07
ibasan <ibarakihirosi@gmail.com>
2026-06-29 16:33:45 +0900

    add meta charset UTF-8

--- nadeshiko_editer/google_copy.js
+++ nadeshiko_editer/google_copy.js
@@ -1,5 +1,5 @@
 const get_googlesite_copy=()=>{
 	const domain='https://github.kasumigaura.work/nadeshiko_editer/';
 	
-	return "<html><body><div id='output'><div style='display:none' id='error'></div><div style='display:none' id='text_output'></div><div style='display:none' id='result_div'></div><canvas style='display:none' id='result_canvas' width='500' height='400'></canvas></div><script type='text/javascript' src='"+domain+"load.js'></script><script id='main' type='なでしこ'>"+code_getText()+"</script><script>setTimeout(()=>{autorun_code=document.getElementById('main').innerText;}, 0);</script></body></html>";
+	return "<html><head><meta charset='UTF-8' /></head><body><div id='output'><div style='display:none' id='error'></div><div style='display:none' id='text_output'></div><div style='display:none' id='result_div'></div><canvas style='display:none' id='result_canvas' width='500' height='400'></canvas></div><script type='text/javascript' src='"+domain+"load.js'></script><script id='main' type='なでしこ'>"+code_getText()+"</script><script>setTimeout(()=>{autorun_code=document.getElementById('main').innerText;}, 0);</script></body></html>";
 }
